6891288a99e3538fac595e49a6cf5c52fca0d79e
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-02-12 13:27:33 -0500

    wip

--- src/excercies/lecture1/hangman.rs
+++ src/excercies/lecture1/hangman.rs
@@ -1,20 +1,44 @@
-use std::io::{self, Read, Write};
+use rand::{prelude::IteratorRandom, thread_rng};
+use std::io::{self, Write};
 
-fn hangman(mut attempts: i32) -> io::Result<(i32)> {
+const ANSWERS: [&'static str; 5] = ["dog", "cat", "monkey", "spiderman", "chicago"];
+
+#[derive(Clone, Copy)]
+struct WordToCheck {
+    word: &'static str,
+    attempts: i32,
+}
+impl WordToCheck {
+    fn new(word: &'static str, attempts: i32) -> Self {
+        Self { word, attempts }
+    }
+}
+
+fn hangman(mut container: WordToCheck) -> io::Result<(i32)> {
+    let WordToCheck { word, mut attempts } = container;
+    println!("{}", &word);
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer)?;
-    if buffer.trim() == "yes" {
+    let response = buffer.trim();
+    dbg!(word.contains(response));
+    if !word.contains(response) {
         attempts -= 1;
-    }
-    io::stdout().write_all(buffer.to_uppercase().as_bytes())?;
+    };
+    io::stdout().write_all("_".repeat(word.len()).as_bytes())?;
     Ok(attempts)
 }
 
 pub fn warhammer() {
-    println!("Welcome");
-    let mut attempts = 5;
-    while attempts != 0 {
-        attempts = hangman(attempts).unwrap();
+    println!("Welcome to hangman");
+    println!("Guess the word");
+    let mut random = thread_rng();
+    let word = ANSWERS.iter().choose(&mut random).unwrap();
+    let mut container = WordToCheck::new(word, 5);
+
+    let underscore = "_".repeat(container.word.len());
+    println!("{}", underscore);
+    while container.attempts != 0 {
+        container.attempts = hangman(container).unwrap();
     }
     println!("Game over");
 }
