0fe41882e7013e0a4c40c05a9b4ce4e21c4af427
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-02-12 05:44:31 -0500

    wip hangman

--- src/excercies/lecture1/hangman.rs
+++ src/excercies/lecture1/hangman.rs
@@ -1,15 +1,20 @@
 use std::io::{self, Read, Write};
 
-fn hangman() -> io::Result<()> {
-    let mut buffer = "".to_string();
-    io::stdin().read_to_string(&mut buffer)?;
+fn hangman(mut attempts: i32) -> io::Result<(i32)> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    if buffer.trim() == "yes" {
+        attempts -= 1;
+    }
     io::stdout().write_all(buffer.to_uppercase().as_bytes())?;
-    Ok(())
+    Ok(attempts)
 }
 
 pub fn warhammer() {
     println!("Welcome");
-    loop {
-        hangman().unwrap();
+    let mut attempts = 5;
+    while attempts != 0 {
+        attempts = hangman(attempts).unwrap();
     }
+    println!("Game over");
 }
