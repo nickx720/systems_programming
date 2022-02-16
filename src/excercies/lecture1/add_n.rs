b2b80eb827b5a02fdd6e38a305084e9514c8f60b
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-02-16 03:43:21 -0500

    completed part2 add_nin place

--- src/excercies/lecture1/add_n.rs
+++ src/excercies/lecture1/add_n.rs
@@ -1,6 +1,16 @@
 fn implement_add_n(vector_of_numbers: Vec<i32>, n: i32) -> Vec<i32> {
     vector_of_numbers.iter().map(|item| item + n).collect()
 }
+
+fn implement_add_n_in_place(vector_of_numbers: &mut Vec<i32>, n: i32) {
+    for index in 0..vector_of_numbers.len() {
+        vector_of_numbers[index] += n;
+    }
+}
+
 pub fn add_n() {
     println!("{:?}", implement_add_n(vec![1, 2, 3], 2));
+    let mut sample = vec![2, 3, 4];
+    implement_add_n_in_place(&mut sample, 5);
+    println!("{:?}", sample);
 }
