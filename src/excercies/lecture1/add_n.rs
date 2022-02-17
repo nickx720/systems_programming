f65367e14b8f55751a44e7486c02a1e7811a5e32
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-02-17 03:30:25 -0500

    rust dedup

--- src/excercies/lecture1/add_n.rs
+++ src/excercies/lecture1/add_n.rs
@@ -1,3 +1,4 @@
+use std::{collections::HashSet, iter::FromIterator};
 fn implement_add_n(vector_of_numbers: Vec<i32>, n: i32) -> Vec<i32> {
     vector_of_numbers.iter().map(|item| item + n).collect()
 }
@@ -7,10 +8,22 @@ fn implement_add_n_in_place(vector_of_numbers: &mut Vec<i32>, n: i32) {
         vector_of_numbers[index] += n;
     }
 }
+// Implement dedup that removes duplicate elements from a vector in-place (i.e. modifies v directly). If an element is repeated anywhere in the vector, you should keep the element that appears first. You may want to use a HashSet for this.
+
+fn implement_dedup_in_place(vector_of_numbers: Vec<i32>) -> Vec<i32> {
+    let mut set: HashSet<i32> = HashSet::new();
+    for item in vector_of_numbers {
+        set.insert(item);
+    }
+    Vec::from_iter(set)
+}
 
 pub fn add_n() {
     println!("{:?}", implement_add_n(vec![1, 2, 3], 2));
     let mut sample = vec![2, 3, 4];
     implement_add_n_in_place(&mut sample, 5);
     println!("{:?}", sample);
+    let duplicate = vec![1, 1, 1, 2, 2, 3, 4];
+    let duplicate = implement_dedup_in_place(duplicate);
+    println!("{:?}", duplicate);
 }
