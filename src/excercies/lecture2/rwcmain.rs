e70349c90169ef08d7db36dd5b04b06998b03bad
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-03 03:46:11 -0500

    todo rwc

---
+++ src/excercies/lecture2/rwcmain.rs
@@ -0,0 +1,10 @@
+use std::{env, process};
+
+pub fn rwcmain() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Too few arguments.");
+        process::exit(1);
+    }
+    let filename = &args[1];
+}
