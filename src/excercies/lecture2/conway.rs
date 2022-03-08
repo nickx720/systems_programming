36a6b2e35c7ca7d00d6dbe246fd88f0847821bb7
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-08 03:46:23 -0500

    added conway logic

--- src/excercies/lecture2/conway.rs
+++ src/excercies/lecture2/conway.rs
@@ -1,30 +1,68 @@
-/// https://guide.nannou.cc/tutorials.html
-/// https://reberhardt.com/cs110l/spring-2020/assignments/week-2-exercises/
 use nannou::prelude::*;
+use std::collections::{HashMap, HashSet};
+/// https://guide.nannou.cc/tutorials.html
+/// https://rosettacode.org/wiki/Conway%27s_Game_of_Life#Rust
+
+type Cell = (i32, i32);
+type Colony = HashSet<Cell>;
+
+fn neighbours(&(x, y): &Cell) -> Vec<Cell> {
+    vec![
+        (x - 1, y - 1),
+        (x, y - 1),
+        (x + 1, y - 1),
+        (x - 1, y),
+        (x + 1, y),
+        (x - 1, y + 1),
+        (x, y + 1),
+        (x + 1, y + 1),
+    ]
+}
+
+fn neighbour_counts(col: &Colony) -> HashMap<Cell, i32> {
+    let mut ncnts = HashMap::new();
+    for cell in col.iter().flat_map(neighbours) {
+        *ncnts.entry(cell).or_insert(0) += 1;
+    }
+    ncnts
+}
+
+fn generation(col: Colony) -> Colony {
+    neighbour_counts(&col)
+        .into_iter()
+        .filter_map(|(cell, cnt)| match (cnt, col.contains(&cell)) {
+            (2, true) | (3, ..) => Some(cell),
+            _ => None,
+        })
+        .collect()
+}
+
+fn life(init: Vec<Cell>, iters: i32, width: i32, height: i32) {
+    let mut col: Colony = init.into_iter().collect();
+    for i in 0..iters + 1 {
+        println!("({i})");
+        if i != 0 {
+            col = generation(col);
+        }
+        // Initiate drawing here
+        nannou::sketch(view).run();
+    }
+}
+
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+
+    draw.background().color(CORNFLOWERBLUE);
+
+    let win = app.window_rect();
+    draw.tri()
+        .points(win.bottom_left(), win.top_left(), win.top_right())
+        .color(VIOLET);
+
+    draw.to_frame(app, &frame).unwrap();
+}
 
 pub fn main() {
-    nannou::app(model).run();
-}
-
-struct Model {
-    _window: WindowId,
-}
-fn model(app: &App) -> Model {
-    let _window = app
-        .new_window()
-        .size(512, 512)
-        .title("Conway")
-        .view(view)
-        .event(event)
-        .build()
-        .unwrap();
-    Model { _window }
-}
-
-fn event(_app: &App, _model: &mut Model, event: WindowEvent) {
-    println!("{event:?}");
-}
-
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(CORNFLOWERBLUE);
+    let blinker = vec![(1, 0), (1, 1), (1, 2)];
+    life(blinker, 3, 3, 3);
 }
