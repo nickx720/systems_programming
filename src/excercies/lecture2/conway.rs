78e4e499a466af0dd8f46b6f9dea37aee9ae84cf
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-10 03:53:35 -0500

    wip

--- src/excercies/lecture2/conway.rs
+++ src/excercies/lecture2/conway.rs
@@ -2,7 +2,7 @@ use nannou::prelude::*;
 use std::collections::{HashMap, HashSet};
 /// https://guide.nannou.cc/tutorials.html
 /// https://rosettacode.org/wiki/Conway%27s_Game_of_Life#Rust
-
+// Todo Return vec from life with all the co-ordinates
 type Cell = (i32, i32);
 type Colony = HashSet<Cell>;
 
@@ -63,10 +63,10 @@ fn print_colony(col: &Colony, width: i32, height: i32) {
 }
 
 pub fn main() {
-    let blinker = vec![(1, 0), (1, 1), (1, 2)];
-    life(blinker, 3, 3, 3);
-    let glider = vec![(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)];
-    life(glider, 20, 8, 8);
+    // let blinker = vec![(1, 0), (1, 1), (1, 2)];
+    // life(blinker, 3, 3, 3);
+    // let glider = vec![(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)];
+    // life(glider, 20, 8, 8);
     nannou::app(model).update(update).run();
 }
 
@@ -75,7 +75,13 @@ struct Model {
 }
 
 fn model(app: &App) -> Model {
-    let _window = app.new_window().view(view).build().unwrap();
+    let _window = app
+        .new_window()
+        .title("Life of Conway")
+        .event(event_test)
+        .view(view)
+        .build()
+        .unwrap();
     Model { _window }
 }
 
@@ -85,11 +91,15 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(PLUM);
     // https://guide.nannou.cc/tutorials/basics/window-coordinates.html
-    for y in 0..10 {
-        draw.ellipse()
-            .x_y(0.0, y as f32)
-            .w_h(100.0, 100.0)
-            .color(STEELBLUE);
+    for y in (10..100).step_by(8) {
+        let (x, y) = (y as f32, y as f32);
+        draw.ellipse().x_y(x, y).w_h(10.0, 10.0).color(STEELBLUE);
     }
     draw.to_frame(app, &frame).unwrap();
+}
+
+// Can we use the hover event?
+
+fn event_test(_app: &App, _model: &mut Model, event: WindowEvent) {
+    println!("window a is {event:?}");
 }
