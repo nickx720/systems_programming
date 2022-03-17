184b19fcfa8464aa24b811dd7b5ca26e1a138443
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-17 03:37:11 -0400

    Wip for printing process

---
+++ src/excercies/lecture3/inspect-fds/process.rs
@@ -0,0 +1,16 @@
+use crate::open_file::OpenFile;
+#[allow(unused)] // TODO: delete this line for Milestone 3
+use std::fs;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Process {
+    pub pid: usize,
+    pub ppid: usize,
+    pub command: String,
+}
+
+impl Process {
+    pub fn new(pid: usize, ppid: usize, command: String) -> Process {
+        Process { pid, ppid, command }
+    }
+}
--- src/excercies/lecture3/mod.rs
+++ src/excercies/lecture3/mod.rs
@@ -1,2 +1,4 @@
 /// https://github.com/reberhardt7/cs110l-spr-2020-starter-code
 /// https://reberhardt.com/cs110l/spring-2020/assignments/week-3-exercises/
+/// Todo Milestone 2 Print under process
+///
