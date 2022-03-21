2f4936107b7d8ae9c0eb7c8cd9f1e6bd9c76c54e
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-21 04:02:57 -0400

    Finished monday push

--- src/excercies/lecture3/inspect-fds/process.rs
+++ src/excercies/lecture3/inspect-fds/process.rs
@@ -13,6 +13,18 @@ impl Process {
     pub fn new(pid: usize, ppid: usize, command: String) -> Process {
         Process { pid, ppid, command }
     }
+
+    /// This function returns a list of file descriptor numbers for this Process, if that
+    /// information is available (it will return None if the information is unavailable). The
+    /// information will commonly be unavailable if the process has exited. (Zombie processes
+    /// still have a pid, but their resources have already been freed, including the file
+    /// descriptor table.)
+    #[allow(unused)] // TODO: delete this line for Milestone 3
+    pub fn list_fds(&self) -> Option<Vec<usize>> {
+        // TODO: implement for Milestone 3
+        unimplemented!();
+    }
+
     /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
     /// information is available (it returns None otherwise). The information is commonly
     /// unavailable if the process has already exited.
