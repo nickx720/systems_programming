3034712776be136623607466be201812852c9b29
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-04-04 09:41:00 -0400

    Working on milestone 4

---
+++ src/excercies/lecture3/inspect-fds/open_file.rs
@@ -0,0 +1,5 @@
+use regex::Regex;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+#[allow(unused_imports)] // TODO: delete this line for Milestone 4
+use std::{fmt, fs};
