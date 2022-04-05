17e6b65d470980bf2c926f5bce297f30bcf54ad2
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-04-05 09:39:25 -0400

    Wip

--- src/excercies/lecture3/inspect-fds/open_file.rs
+++ src/excercies/lecture3/inspect-fds/open_file.rs
@@ -3,3 +3,8 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 #[allow(unused_imports)] // TODO: delete this line for Milestone 4
 use std::{fmt, fs};
+
+#[allow(unused)] // TODO: delete this line for Milestone 4
+const O_WRONLY: usize = 00000001;
+#[allow(unused)] // TODO: delete this line for Milestone 4
+const O_RDWR: usize = 00000002;
