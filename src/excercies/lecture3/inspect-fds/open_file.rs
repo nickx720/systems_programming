1c2f997f4cdd193246cf0ec273cb722290d2cbc7
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-04-12 10:02:39 -0400

    Milestone 5 completed

--- src/excercies/lecture3/inspect-fds/open_file.rs
+++ src/excercies/lecture3/inspect-fds/open_file.rs
@@ -17,7 +17,6 @@ const COLORS: [&str; 6] = [
     "\x1B[38;5;13m",
     "\x1B[38;5;14m",
 ];
-
 #[allow(unused)] // TODO: delete this line for Milestone 4
 const CLEAR_COLOR: &str = "\x1B[0m";
 
@@ -30,6 +29,7 @@ pub enum AccessMode {
     Write,
     ReadWrite,
 }
+
 impl fmt::Display for AccessMode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // Match operators are very commonly used with enums in Rust. They function similar to
@@ -79,3 +79,119 @@ impl OpenFile {
             String::from(path)
         }
     }
+
+    /// This file takes the contents of /proc/{pid}/fdinfo/{fdnum} for some file descriptor and
+    /// extracts the cursor position of that file descriptor (technically, the position of the
+    /// open file table entry that the fd points to) using a regex. It returns None if the cursor
+    /// couldn't be found in the fdinfo text.
+    #[allow(unused)] // TODO: delete this line for Milestone 4
+    fn parse_cursor(fdinfo: &str) -> Option<usize> {
+        // Regex::new will return an Error if there is a syntactical error in our regular
+        // expression. We call unwrap() here because that indicates there's an obvious problem with
+        // our code, but if this were code for a critical system that needs to not crash, then
+        // we would want to return an Error instead.
+        let re = Regex::new(r"pos:\s*(\d+)").unwrap();
+        Some(
+            re.captures(fdinfo)?
+                .get(1)?
+                .as_str()
+                .parse::<usize>()
+                .ok()?,
+        )
+    }
+
+    /// This file takes the contents of /proc/{pid}/fdinfo/{fdnum} for some file descriptor and
+    /// extracts the access mode for that open file using the "flags:" field contained in the
+    /// fdinfo text. It returns None if the "flags" field couldn't be found.
+    #[allow(unused)] // TODO: delete this line for Milestone 4
+    fn parse_access_mode(fdinfo: &str) -> Option<AccessMode> {
+        // Regex::new will return an Error if there is a syntactical error in our regular
+        // expression. We call unwrap() here because that indicates there's an obvious problem with
+        // our code, but if this were code for a critical system that needs to not crash, then
+        // we would want to return an Error instead.
+        let re = Regex::new(r"flags:\s*(\d+)").unwrap();
+        // Extract the flags field and parse it as octal
+        let flags = usize::from_str_radix(re.captures(fdinfo)?.get(1)?.as_str(), 8).ok()?;
+        if flags & O_WRONLY > 0 {
+            Some(AccessMode::Write)
+        } else if flags & O_RDWR > 0 {
+            Some(AccessMode::ReadWrite)
+        } else {
+            Some(AccessMode::Read)
+        }
+    }
+
+    /// Given a specified process and fd number, this function reads /proc/{pid}/fd/{fdnum} and
+    /// /proc/{pid}/fdinfo/{fdnum} to populate an OpenFile struct. It returns None if the pid or fd
+    /// are invalid, or if necessary information is unavailable.
+    ///
+    /// (Note: whether this function returns Option or Result is a matter of style and context.
+    /// Some people might argue that you should return Result, so that you have finer grained
+    /// control over possible things that could go wrong, e.g. you might want to handle things
+    /// differently if this fails because the process doesn't have a specified fd, vs if it
+    /// fails because it failed to read a /proc file. However, that significantly increases
+    /// complexity of error handling. In our case, this does not need to be a super robust
+    /// program and we don't need to do fine-grained error handling, so returning Option is a
+    /// simple way to indicate that "hey, we weren't able to get the necessary information"
+    /// without making a big deal of it.)
+    #[allow(unused)] // TODO: delete this line for Milestone 4
+    pub fn from_fd(pid: usize, fd: usize) -> Option<OpenFile> {
+        // TODO: implement for Milestone 4
+        unimplemented!();
+    }
+
+    /// This function returns the OpenFile's name with ANSI escape codes included to colorize
+    /// pipe names. It hashes the pipe name so that the same pipe name will always result in the
+    /// same color. This is useful for making program output more readable, since a user can
+    /// quickly see all the fds that point to a particular pipe.
+    #[allow(unused)] // TODO: delete this line for Milestone 5
+    pub fn colorized_name(&self) -> String {
+        if self.name.starts_with("<pipe") {
+            let mut hash = DefaultHasher::new();
+            self.name.hash(&mut hash);
+            let hash_val = hash.finish();
+            let color = COLORS[(hash_val % COLORS.len() as u64) as usize];
+            format!("{}{}{}", color, self.name, CLEAR_COLOR)
+        } else {
+            format!("{}", self.name)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::ps_utils;
+    use std::process::{Child, Command};
+
+    fn start_c_program(program: &str) -> Child {
+        Command::new(program)
+            .spawn()
+            .expect(&format!("Could not find {}. Have you run make?", program))
+    }
+
+    #[test]
+    fn test_openfile_from_fd() {
+        let mut test_subprocess = start_c_program("./multi_pipe_test");
+        let process = ps_utils::get_target("multi_pipe_test").unwrap().unwrap();
+        // Get file descriptor 0, which should point to the terminal
+        let open_file = OpenFile::from_fd(process.pid, 0)
+            .expect("Expected to get open file data for multi_pipe_test, but OpenFile::from_fd returned None");
+        assert_eq!(open_file.name, "<terminal>");
+        assert_eq!(open_file.cursor, 0);
+        assert_eq!(open_file.access_mode, AccessMode::ReadWrite);
+        let _ = test_subprocess.kill();
+    }
+
+    #[test]
+    fn test_openfile_from_fd_invalid_fd() {
+        let mut test_subprocess = start_c_program("./multi_pipe_test");
+        let process = ps_utils::get_target("multi_pipe_test").unwrap().unwrap();
+        // Get file descriptor 30, which should be invalid
+        assert!(
+            OpenFile::from_fd(process.pid, 30).is_none(),
+            "Expected None because file descriptor 30 is invalid"
+        );
+        let _ = test_subprocess.kill();
+    }
+}
