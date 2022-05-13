8671810ca619faf4ed1d467e33d78a3aafdfed58
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-05-13 06:45:15 -0400

    Updating inferior.rs for m3

--- proj1/deet/src/inferior.rs
+++ proj1/deet/src/inferior.rs
@@ -94,4 +94,9 @@ impl Inferior {
         }
         Ok(Status::Restart)
     }
+
+    pub fn print_backtrace(&self) -> Result<(), nix::Error> {
+        println!("Hello World");
+        Ok(())
+    }
 }
