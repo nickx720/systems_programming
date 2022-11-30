450f4844ef3b2f997e8326115518fe205845bee9
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-11-30 07:33:22 -0500

    changes

--- src/excercies/lecture5/mod.rs
+++ src/excercies/lecture5/mod.rs
@@ -1,4 +1,4 @@
-// https://youtu.be/NSJW0BvZI0c?t=1640
+// https://youtu.be/NSJW0BvZI0c?t=1641
 use std::{i32, thread, time};
 // mod borrow;
 fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
